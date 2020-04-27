987da09c95188551ac6aeef34e786b677dc269a7
Hari Govind <grhari1998@gmail.com>
2020-04-26 22:03:15 -0400

    dynamic classeNames for css

--- src/App.js
+++ src/App.js
@@ -41,9 +41,11 @@ class App extends Component {
     });
   };
 
+
   render() {
     const style = {
-      backgroundColor: "white",
+      backgroundColor: "green",
+      color: 'white',
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
@@ -66,11 +68,21 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = 'red';
+    }
+
+    const classes = [];
+    if(this.state.persons.length <= 2){
+      classes.push('red');
+    }
+
+    if(this.state.persons.length<=1){
+      classes.push('bold');
     }
 
     return (
       <div className="App">
-        <h1>Hello World!</h1>
+        <h1 className={classes.join(' ')}>Hello World!</h1>
         <button onClick={this.togglePersonHandler} style={style}>
           Toggle show persons
         </button>
